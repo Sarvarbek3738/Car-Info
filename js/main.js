ab17a6b91fcec31fe20aae4e0ce22b8680c0d008
Sarvarbek <sarvarbekodilxojayev898@gmail.com>
2024-12-25 09:51:14 +0500

    update

--- js/main.js
+++ js/main.js
@@ -4,13 +4,32 @@ const speed = document.querySelector(".car-Speed")
 const prise = document.querySelector(".car-Price")
 const image = document.querySelector(".car-image")
 const button = document.querySelector("#button")
+const allCards = document.querySelector(".allCards")
 
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    document.querySelector("#name").innerHTML = name.value;
-    document.querySelector("#speed").innerHTML = speed.value;
-    document.querySelector("#price").innerHTML = prise.value;
-    document.querySelector("#Image").src = image.value ;
-    document.querySelector(".natija").style.display = "block" ;
-})
 
+    e.preventDefault();
+
+    if(name.value == "" || speed.value == "" || image.value == "" || prise.value == "") {
+        alert("Malumot toliq kritilmagan")
+    } else {
+        allCards.innerHTML +=  `
+        <div class="natija">
+            <div class="result-container">
+                <!-- <h1 class="placeholderMessage" >Ma'lumot kiriting</h1> -->
+                <div id="car-details" class="hidden">
+                    <div class="img"><img id="Image" src="${image.value}" alt="Car image"></div>
+                    <div class="title">
+                        <h3 id="name">${name.value}</h3>
+                        <p><strong>Speed:</strong> <span id="speed">${speed.value}</span> km/h</p>
+                        <p><strong>Price:</strong> $<span id="price">${prise.value}</span></p>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    `
+    }
+    
+    
+})
